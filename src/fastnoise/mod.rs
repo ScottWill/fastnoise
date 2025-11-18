825bd2a1dd916279c3fb352128e024b9f778f0d5
Scott Will <scottdwill@gmail.com>
2025-11-18 06:41:00 -0600

    establish a test

--- src/fastnoise/mod.rs
+++ src/fastnoise/mod.rs
@@ -1,3 +1,4 @@
+mod consts;
 mod fastnoise;
 
 pub use fastnoise::*;
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,6 @@
-use bracket_color::prelude::*;
-// use bracket_noise::prelude::*;
-
-
-use crossterm::queue;
-use crossterm::style::{Color::Rgb, Print, SetForegroundColor};
 use fastnoise::*;
-use std::io::{stdout, Write};
-
-fn print_color(color: RGB, text: &str) {
-    queue!(
-        stdout(),
-        SetForegroundColor(Rgb {
-            r: (color.r * 255.0) as u8,
-            g: (color.g * 255.0) as u8,
-            b: (color.b * 255.0) as u8,
-        })
-    )
-    .expect("Command Fail");
-    queue!(stdout(), Print(text)).expect("Command fail");
-}
 
 fn main() {
-    // let mut rng = RandomNumberGenerator::new();
     let mut noise = FastNoise::seeded(1337);
     noise.set_noise_type(NoiseType::SimplexFractal);
     noise.set_fractal_type(FractalType::Billow);
@@ -31,18 +10,15 @@ fn main() {
     noise.set_fractal_lacunarity(2.0);
     noise.set_frequency(2.0);
 
+    let mut total = 0.0;
+
     for y in 0..50 {
         for x in 0..80 {
             let n = noise.get_noise((x as f32) / 160.0, (y as f32) / 100.0);
-            if n < 0.0 {
-                print_color(RGB::from_f32(0.0, 0.0, 1.0 - (0.0 - n)), "░");
-            } else {
-                print_color(RGB::from_f32(0.0, n, 0.0), "░");
-            }
+            total += n;
         }
-        print_color(RGB::named(WHITE), "\n");
     }
 
-    print_color(RGB::named(WHITE), "\n");
-    stdout().flush().expect("Flush Fail");
+    assert_eq!(-983.51575, total);
+    println!("\nTest Passed =)\n");
 }
